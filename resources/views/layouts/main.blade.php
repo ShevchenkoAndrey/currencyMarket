5f75609307575a2df2b538f40024022998662ae2
Andrey Shevchenko <shevochk@mail.ua>
2016-08-16 20:48:23 +0300

    add some depences and bower

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
 
-    {!! Html::style('css/app.css') !!}
-
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
+    {!! Html::style('css/app.css') !!}
     {!! Html::script('js/custom.js') !!}
 
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,37 @@
 @extends('layouts/main')
 
-{!! Html::style('css/animate.css') !!}
-
 @section('title')
   My Courtyard
 @endsection
 
 @section('content')
-<div class="homepage-block">
-    <span class="pulse animated welcomeMessage">My Courtyard</span>
+<div class="homepage-block row">
+    <span class="pulse animated welcomeMessage col-lg-12">My Courtyard</span>
+
+    <div class="col-lg-3 col-lg-offset-3 fadein animated"><a href="#" data-toggle="modal" data-target="#registrationModal">Registration</a></div>
+    <div class="col-lg-3 fadein animated"><a href="#">Login</a></div>
 </div>
 @endsection
+
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Modal Header</h4>
+            </div>
+            <div class="modal-body">
+                <p>Some text in the modal.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+
+    </div>
+</div>
